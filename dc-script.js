3fd1726984e0239db16482510d220f1678c29e23
kazunorik <38503352+kazunorik@users.noreply.github.com>
2020-05-17 11:28:08 -0300

    empty boss name

--- dc-script.js
+++ dc-script.js
@@ -44,14 +44,16 @@ function boss_drop_minus(boss){
 
 function addnewboss(){
     var newbossname = $("newboss").value
-    data_json.push({"name": newbossname, "kills": 0, "drops": 0})
-    localStorage.setItem('data', JSON.stringify(data_json));
-    i = data_json.length - 1
-    var newrow = document.createElement("TR")
-    newrow.innerHTML = "<th><button class=\"delbut\" onclick=\"removeboss(" + i + ")\">x</button></th><th class=\"bossname\">" + data_json[i].name + "</th><th><button onclick=\"boss_count(" + i + ")\">+</button><a id=\"count" + i + "\">" + data_json[i].kills + "</a><button onclick=\"boss_count_minus(" + i + ")\">-</button></th><th><button onclick=\"boss_drop(" + i + ")\">+</button><a id=\"drop" + i + "\">" + data_json[i].drops + "</a><button onclick=\"boss_drop_minus(" + i + ")\">-</button></th>"
-    newrow.id = "boss" + i
-    $("bosses").appendChild(newrow)
-    $("newboss").value = ''
+    if (newbossname != ''){
+        data_json.push({"name": newbossname, "kills": 0, "drops": 0})
+        localStorage.setItem('data', JSON.stringify(data_json));
+        i = data_json.length - 1
+        var newrow = document.createElement("TR")
+        newrow.innerHTML = "<th><button class=\"delbut\" onclick=\"removeboss(" + i + ")\">x</button></th><th class=\"bossname\">" + data_json[i].name + "</th><th><button onclick=\"boss_count(" + i + ")\">+</button><a id=\"count" + i + "\">" + data_json[i].kills + "</a><button onclick=\"boss_count_minus(" + i + ")\">-</button></th><th><button onclick=\"boss_drop(" + i + ")\">+</button><a id=\"drop" + i + "\">" + data_json[i].drops + "</a><button onclick=\"boss_drop_minus(" + i + ")\">-</button></th>"
+        newrow.id = "boss" + i
+        $("bosses").appendChild(newrow)
+        $("newboss").value = ''
+    }
 }
 
 function buildtable(){
